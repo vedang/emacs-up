4e5ada7416774891b57a17b514262e51975c50fd
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 14:06:52 +0530

    Add all subdirs under .emacs.d to load-path

--- init.el
+++ init.el
@@ -5,6 +5,16 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+;;; From nflath.com
+;;; add all subdirs under "~/.emacs.d/" to load-path
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+    (let* ((my-lisp-dir "~/.emacs.d/")
+           (default-directory my-lisp-dir)
+           (orig-load-path load-path))
+      (setq load-path (cons my-lisp-dir nil))
+      (normal-top-level-add-subdirs-to-load-path)
+      (nconc load-path orig-load-path)))
+
 ;;; Load ELPA
 (require 'package)
 (package-initialize)
