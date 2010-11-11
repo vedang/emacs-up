a8e04f961151f171587a508468b3c6628789b3cc
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 19:03:10 +0530

    Sugar for slime - color in the clojure repl and automatic starting of paredit

--- configuration/lisp-mode-config.el
+++ configuration/lisp-mode-config.el
@@ -64,4 +64,53 @@ context-help to false"
   (add-hook
    (intern (concat (symbol-name x) "-mode-hook")) 'run-coding-hook))
 
+;;; Slime
+(require 'slime-autoloads)
+
+(add-hook 'lisp-mode-hook 'turn-on-slime)
+(add-hook 'inferior-lisp-mode-hook 'turn-on-slime)
+
+;;; paredit
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code."
+  t)
+
+(add-hook 'slime-mode-hook 'turn-on-paredit)
+(add-hook 'lisp-mode-hook 'turn-on-paredit)
+(add-hook 'slime-repl-mode-hook 'turn-on-paredit)
+(add-hook 'slime-connected-hook 'turn-on-paredit)
+
+;;; http://stackoverflow.com/questions/2474804/is-there-a-colored-repl-for-clojure
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (clojure-font-lock-setup)))
+
 (provide 'lisp-mode-config)
