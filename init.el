84c1ee8831c166366449bc11a0ae9f67fb94b801
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 14:05:20 +0530

    Load elpa

--- init.el
+++ init.el
@@ -4,3 +4,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;;; Load ELPA
+(require 'package)
+(package-initialize)
