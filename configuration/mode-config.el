31a6dafd5ae7fe6b8c36b52ff2cd2a75b7c63d7a
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 14:30:43 +0530

    Added configuration for cscope

--- configuration/mode-config.el
+++ configuration/mode-config.el
@@ -2,3 +2,11 @@
 (require 'package)
 (package-initialize)
 (require 'elpa-config)
+
+;;; Better dired
+(require 'dired-x)
+
+;;; cscope - awesome search in c/c++ files
+(require 'xcscope)
+(require 'cscope-mode-config)
+
