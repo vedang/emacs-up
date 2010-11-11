569e02b4dd6161c67ef5c11f32d6c8ead7ec6b72
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 15:37:27 +0530

    More utility functions from emacs-starter-kit

--- configuration/utility-functions.el
+++ configuration/utility-functions.el
@@ -181,5 +181,41 @@ Subsequent calls expands the selection to larger semantic unit."
   "Enable things that are convenient across all coding buffers."
   (run-hooks 'coding-hook))
 
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+;;; recompile config directory; regen autoloads
+(defun recompile-init ()
+  "Byte-compile all your dotfiles again."
+  (interactive)
+  (byte-recompile-directory dotfiles-dir 0)
+  ;; TODO: remove elpa-to-submit once everything's submitted.
+  (byte-recompile-directory (concat dotfiles-dir "elpa-to-submit/") 0))
+
+(defun regen-autoloads (&optional force-regen)
+  "Regenerate the autoload definitions file if necessary and load it."
+  (interactive "P")
+  (let ((autoload-dir (concat dotfiles-dir "/elpa-to-submit"))
+        (generated-autoload-file autoload-file))
+    (when (or force-regen
+              (not (file-exists-p autoload-file))
+              (some (lambda (f) (file-newer-than-file-p f autoload-file))
+                    (directory-files autoload-dir t "\\.el$")))
+      (message "Updating autoloads...")
+      (let (emacs-lisp-mode-hook)
+        (update-directory-autoloads autoload-dir))))
+  (load autoload-file))
 
 (provide 'utility-functions)
