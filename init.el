c5d8444029abe004d1b5e6aa6653bc2b71df37ef
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 14:00:32 +0530

    turn the gui off

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
 ;;; Root emacs configuration file.
+
+;;; No GUI
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
