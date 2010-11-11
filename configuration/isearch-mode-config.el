89c1cde9af0af2fb24b60034b525f381d8667e6a
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 15:55:18 +0530

    regex isearch should be the default

--- configuration/isearch-mode-config.el
+++ configuration/isearch-mode-config.el
@@ -25,4 +25,10 @@
 				       'vedang/isearch-yank-word-or-char-from-beginning
 				       isearch-mode-map)))
 
+;; Use regex searches by default.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (provide 'isearch-mode-config)
