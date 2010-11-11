4c100a53618c5befd7aacb8510bce8e62cefa5ad
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-11 15:40:38 +0530

    Defined some global values which are used by functions later

--- init.el
+++ init.el
@@ -15,6 +15,13 @@
       (normal-top-level-add-subdirs-to-load-path)
       (nconc load-path orig-load-path)))
 
+;;; Some global defs
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
 ;;; Require common stuff
 (require 'cl)
 (require 'uniquify)
