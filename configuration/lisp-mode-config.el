b1f9a453aa82fba5f367844e914f6ddbaec5bfbc
Vedang Manerikar <ved.manerikar@gmail.com>
2010-11-12 14:53:53 +0530

    font lock for slime repl

--- configuration/lisp-mode-config.el
+++ configuration/lisp-mode-config.el
@@ -113,4 +113,21 @@ context-help to false"
           (lambda ()
             (clojure-font-lock-setup)))
 
+(defadvice slime-repl-emit (after sr-emit-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (add-text-properties slime-output-start slime-output-end
+                         '(font-lock-face slime-repl-output-face
+                                          rear-nonsticky (font-lock-face)))))
+
+(defadvice slime-repl-insert-prompt (after sr-prompt-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (let ((inhibit-read-only t))
+      (add-text-properties slime-repl-prompt-start-mark (point-max)
+                           '(font-lock-face slime-repl-prompt-face
+                                            rear-nonsticky
+                                            (slime-repl-prompt
+                                             read-only
+                                             font-lock-face
+                                             intangible))))))
+
 (provide 'lisp-mode-config)
