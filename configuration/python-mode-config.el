df0ffa14cb235147c6d3f5e1ee404d0a8a0ce4d7
Vedang Manerikar <ved.manerikar@gmail.com>
2010-12-19 01:14:20 +0530

    load pymacs and rope lazily

--- configuration/python-mode-config.el
+++ configuration/python-mode-config.el
@@ -1,6 +1,15 @@
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
+;;; settings for python
+
+(defun load-ropemacs ()
+  "Load pymacs and ropemacs"
+  (interactive)
+  (require 'pymacs)
+  (pymacs-load "ropemacs" "rope-")
+
+  (setq ropemacs-confirm-saving 'nil)
+  (setq ropemacs-enable-autoimport t))
+(add-hook 'python-mode-hook 'load-ropemacs)
+
 (setq python-check-command "pyflakes")
 
 (provide 'python-mode-config)
