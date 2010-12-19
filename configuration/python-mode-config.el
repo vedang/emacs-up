3b7837255d5b5ad35a8f22e9f2c63c7f66c2127a
Vedang Manerikar <ved.manerikar@gmail.com>
2010-12-19 18:16:21 +0530

    ropemacs integration with auto-complete

--- configuration/python-mode-config.el
+++ configuration/python-mode-config.el
@@ -12,4 +12,30 @@
 
 (setq python-check-command "pyflakes")
 
+;;; ropemacs Integration with auto-completion
+;;; from github.com/gabrielelanaro
+(defun ac-ropemacs-candidates ()
+  (mapcar (lambda (completion)
+            (concat ac-prefix completion))
+          (rope-completions)))
+
+(ac-define-source nropemacs
+  '((candidates . ac-ropemacs-candidates)
+    (symbol . "p")))
+
+(ac-define-source nropemacs-dot
+  '((candidates . ac-ropemacs-candidates)
+    (symbol . "p")
+    (prefix . c-dot)
+    (requires . 0)))
+
+(defun ac-nropemacs-setup ()
+  (setq ac-sources (append '(ac-source-nropemacs
+                             ac-source-nropemacs-dot) ac-sources)))
+(defun ac-python-mode-setup ()
+  (add-to-list 'ac-sources 'ac-source-yasnippet))
+
+(add-hook 'python-mode-hook 'ac-python-mode-setup)
+(add-hook 'rope-open-project-hook 'ac-nropemacs-setup)
+
 (provide 'python-mode-config)
