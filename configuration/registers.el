b33cab841516286b7dc8695ffc51db461298f8b5
Vedang Manerikar <ved.manerikar@gmail.com>
2011-03-17 16:31:27 +0530

    org-mode-config file should have fast-access through a register

--- configuration/registers.el
+++ configuration/registers.el
@@ -2,7 +2,8 @@
 ;;; pre-defining registers
 ;;; (from emacs-starter-kit)
 
-(dolist (r `((?i (file . ,(concat dotfiles-dir "init.el")))))
+(dolist (r `((?i (file . ,(concat dotfiles-dir "init.el")))
+             (?o (file . ,(concat dotfiles-dir "configuration/org-mode-config.el")))))
   (set-register (car r) (cadr r)))
 
 (provide 'registers)
