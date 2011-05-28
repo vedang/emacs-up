56b19d4081e4d2e25b2cdd82481ea3c54eeeac14
Vedang Manerikar <ved.manerikar@gmail.com>
2011-05-28 20:12:24 +0530

    moved configuration for clojure and emacs-lisp into their own files

---
+++ configuration/clojure-mode-config.el
@@ -0,0 +1,57 @@
+;;; Settings for clojure
+
+(require 'clojure-mode)
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\>\\)"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(define-clojure-indent
+     (describe 'defun)
+     (testing 'defun)
+     (given 'defun)
+     (using 'defun)
+     (with 'defun)
+     (it 'defun)
+     (do-it 'defun)))
+
+
+;;; http://stackoverflow.com/questions/2474804/is-there-a-colored-repl-for-clojure
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (clojure-font-lock-setup)))
+
+
+(provide 'clojure-mode-config)
--- configuration/lisp-mode-config.el
+++ configuration/lisp-mode-config.el
@@ -1,121 +1,12 @@
-;;; Settings for Lisp
-
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(define-key emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
-
-(defun compile-el-on-save ()
-  "If saving an elisp file, byte-compile it."
-  (add-hook 'after-save-hook 'emacs-lisp-byte-compile t t))
-
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (compile-el-on-save)))
-
-(defun rgr/toggle-context-help()
-  "Turn on or off the context help.
-Note that if ON and you hide the help buffer then you need to
-manually reshow it. A double toggle will make it reappear"
-  (interactive)
-  (with-current-buffer (help-buffer)
-    (unless (local-variable-p 'context-help)
-      (set (make-local-variable 'context-help) t))
-    (if (setq context-help (not context-help))
-        (progn
-          (if (not (get-buffer-window (help-buffer)))
-              (display-buffer (help-buffer)))))
-    (message "Context help %s" (if context-help "ON" "OFF"))))
-
-(defun rgr/context-help()
-  "Display function or variable at point in *Help* buffer if visible.
-Default behaviour can be turned off by setting the buffer local
-context-help to false"
-  (interactive)
-
-  (let(( rgr-symbol (symbol-at-point))) ; symbol-at-point http://www.emacswiki.org/cgi-bin/wiki/thingatpt%2B.el
-    (with-current-buffer (help-buffer)
-      (unless (local-variable-p 'context-help)
-        (set (make-local-variable 'context-help) t))
-      (if (and context-help (get-buffer-window (help-buffer))
-               rgr-symbol)
-          (if (fboundp  rgr-symbol)
-              (describe-function rgr-symbol)
-            (if (boundp  rgr-symbol) (describe-variable rgr-symbol)))))))
-
-(defadvice eldoc-print-current-symbol-info
-  (around eldoc-show-c-tag activate)
-  (cond
-   ((eq major-mode 'emacs-lisp-mode)(rgr/context-help) ad-do-it)
-   ((eq major-mode 'lisp-interaction-mode)(rgr/context-help) ad-do-it)
-   ((eq major-mode 'apropos-mode)(rgr/context-help) ad-do-it)
-   (t ad-do-it)))
-
-(global-set-key (kbd "C-c h") 'rgr/toggle-context-help)
-
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("(\\(fn\\>\\)"
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "ƒ")
-                               nil))))))
-
-(eval-after-load 'clojure-mode
-  '(define-clojure-indent
-     (describe 'defun)
-     (testing 'defun)
-     (given 'defun)
-     (using 'defun)
-     (with 'defun)
-     (it 'defun)
-     (do-it 'defun)))
-
 ;;; Slime
 (require 'slime-autoloads)
 
 (add-hook 'lisp-mode-hook 'turn-on-slime)
 (add-hook 'inferior-lisp-mode-hook 'turn-on-slime)
-
-;;; paredit
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code."
-  t)
-
 (add-hook 'slime-mode-hook 'turn-on-paredit)
-(add-hook 'lisp-mode-hook 'turn-on-paredit)
 (add-hook 'slime-repl-mode-hook 'turn-on-paredit)
 (add-hook 'slime-connected-hook 'turn-on-paredit)
 
-;;; http://stackoverflow.com/questions/2474804/is-there-a-colored-repl-for-clojure
-(defun clojure-font-lock-setup ()
-  (interactive)
-  (set (make-local-variable 'lisp-indent-function)
-       'clojure-indent-function)
-  (set (make-local-variable 'lisp-doc-string-elt-property)
-       'clojure-doc-string-elt)
-  (set (make-local-variable 'font-lock-multiline) t)
-
-  (add-to-list 'font-lock-extend-region-functions
-               'clojure-font-lock-extend-region-def t)
-
-  (when clojure-mode-font-lock-comment-sexp
-    (add-to-list 'font-lock-extend-region-functions
-                 'clojure-font-lock-extend-region-comment t)
-    (make-local-variable 'clojure-font-lock-keywords)
-    (add-to-list 'clojure-font-lock-keywords
-                 'clojure-font-lock-mark-comment t)
-    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
-
-  (setq font-lock-defaults
-        '(clojure-font-lock-keywords ; keywords
-          nil nil
-          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
-          nil
-          (font-lock-mark-block-function . mark-defun)
-          (font-lock-syntactic-face-function
-           . lisp-font-lock-syntactic-face-function))))
-
-(add-hook 'slime-repl-mode-hook
-          (lambda ()
-            (clojure-font-lock-setup)))
 
 (defadvice slime-repl-emit (after sr-emit-ad activate)
   (with-current-buffer (slime-output-buffer)
