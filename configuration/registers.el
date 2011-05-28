39a31f6151f68db3309586a1cec1506fcee3138c
Vedang Manerikar <ved.manerikar@gmail.com>
2011-05-29 00:23:45 +0530

    added mode-config file to a register since I have to open it so often

--- configuration/registers.el
+++ configuration/registers.el
@@ -3,7 +3,8 @@
 ;;; (from emacs-starter-kit)
 
 (dolist (r `((?i (file . ,(concat dotfiles-dir "init.el")))
-             (?o (file . ,(concat dotfiles-dir "configuration/org-mode-config.el")))))
+             (?o (file . ,(concat dotfiles-dir "configuration/org-mode-config.el")))
+             (?m (file . ,(concat dotfiles-dir "configuration/mode-config.el")))))
   (set-register (car r) (cadr r)))
 
 (provide 'registers)
