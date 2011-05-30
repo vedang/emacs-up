0147570b99dcf156f0fa46aecab2d258169d8ccd
Vedang Manerikar <ved.manerikar@gmail.com>
2011-05-30 13:24:20 +0530

    wrote flymake config for latex files

--- configuration/flymake-config.el
+++ configuration/flymake-config.el
@@ -1,5 +1,6 @@
 ;;; settings for flymake
 (when (load "flymake" t)
+  ;; Flymake for Python
   (defun flymake-pyflakes-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
@@ -8,17 +9,30 @@
                         (file-name-directory buffer-file-name))))
       (list "pycheckers"  (list local-file))))
 
+  ;; Flymake for Erlang
   (defun flymake-erlang-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
            (local-file (file-relative-name temp-file
-                                           (file-name-directory buffer-file-name))))
+                        (file-name-directory buffer-file-name))))
       (list "erlcheckers" (list local-file))))
+
+  ;; Flymake for LaTeX
+  (defun flymake-get-tex-args (file-name)
+    (list "chktex" (list "-g0" "-r" "-l"
+                         (expand-file-name (concat dotfiles-dir
+                          "plugins/latex/chktexcheckers"))
+                         "-I" "-q" "-v0" file-name)))
+
 
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init))
   (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.erl\\'" flymake-erlang-init)))
+               '("\\.erl\\'" flymake-erlang-init))
+  (push
+   '("^\\(\.+\.tex\\):\\([0-9]+\\):\\([0-9]+\\):\\(.+\\)"
+     1 2 3 4) flymake-err-line-patterns))
+
 
 (load-library "flymake-cursor")
 (global-set-key [f3] 'flymake-goto-prev-error)
