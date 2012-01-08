d4ec5231b35681430adbee88973f55e6a030387b
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-08 17:57:31 +0530

    Activate occur from inside isearch. This is another one of those "I don't know how I survived without this" gems.

--- configuration/isearch-mode-config.el
+++ configuration/isearch-mode-config.el
@@ -1,3 +1,21 @@
+;;; isearch-mode-config.el --- Extra spice for isearch
+;;; Author: Vedang Manerikar
+;;; Created on: 08 Jan 2012
+;;; Time-stamp: "2012-01-08 18:00:22 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
+
+
 ;; From EmacsWiki
 ;; Move to beginning of word before yanking word in isearch-mode.
 ;; Make C-s C-w and C-r C-w act like Vim's g* and g#, keeping Emacs'
@@ -24,6 +42,15 @@
             (substitute-key-definition 'isearch-yank-word-or-char
                                        'vedang/isearch-yank-word-or-char-from-beginning
                                        isearch-mode-map)))
+
+
+;; Activate occur easily inside isearch
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp
+                 isearch-string
+               (regexp-quote isearch-string))))))
 
 
 ;; Use regex searches by default.
