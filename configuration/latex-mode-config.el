4b4106835005a6fbae892c31120ad5d74f26cf55
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-08 19:04:44 +0530

    indentation, added header

--- configuration/latex-mode-config.el
+++ configuration/latex-mode-config.el
@@ -1,5 +1,20 @@
-;; Settings for LaTeX mode
-; ==============================================================================
+;;; latex-mode-config.el --- Settings for LaTeX mode
+;;; Author: Vedang Manerikar
+;;; Created on: 08 Jan 2012
+;;; Time-stamp: "2012-01-08 19:04:08 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
+
 
 (defun latex-mode-settings ()
   "my settings for LaTeX mode"
@@ -13,6 +28,5 @@
 (setq TeX-auto-save t
       TeX-save-query nil
       TeX-parse-self t)
-; ==============================================================================
 
 (provide 'latex-mode-config)
