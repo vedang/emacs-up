4f2c29a5d9f7bdb8ceaed6441bbe7224a1ca3419
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-08 19:01:13 +0530

    package.el comes in-built with 24. Also, it is now simpler to install pre-defined libraries

--- configuration/elpa-config.el
+++ configuration/elpa-config.el
@@ -1,43 +1,42 @@
-;;; Configuration for ELPA: the emacs package manager
+;;; elpa-config.el --- Configuration for ELPA: the emacs package manager
+;;; Author: Vedang Manerikar
+;;; Created on: 08 Jan 2012
+;;; Time-stamp: "2012-01-08 18:59:12 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
+
 
 (require 'package)
-(package-initialize)
-(add-to-list 'package-archives ;; External repos
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
+;;; External repos
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("ELPA" . "http://tromey.com/elpa/") t)
+
+(package-initialize)
 
 ;;; From emacs-starter-kit
-(defvar starter-kit-packages (list 'magit
-                                   'gist
-                                   'erc
-                                   'clojure-mode)
+(defvar starter-packages '(magit gist erc auctex paredit markdown-mode
+                                 clojure-mode)
   "Libraries that should be installed by default.")
 
-(defun starter-kit-elpa-install ()
-  "Install all starter-kit packages that aren't installed."
-  (interactive)
-  (dolist (package starter-kit-packages)
-    (unless (or (member package package-activated-list)
-                (functionp package))
-      (message "Installing %s" (symbol-name package))
-      (package-install package))))
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
-(defun esk-online? ()
-  "See if we're online.
-Windows does not have the network-interface-list function, so we
-just have to assume it's online."
-  ;; TODO how could this work on Windows?
-  (if (and (functionp 'network-interface-list)
-           (network-interface-list))
-      (some (lambda (iface) (unless (equal "lo" (car iface))
-                         (member 'up (first (last (network-interface-info
-                                                   (car iface)))))))
-            (network-interface-list))
-    t))
+(dolist (p starter-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
-;; On your first run, this should pull in all the base packages.
-(when (esk-online?)
-  (unless package-archive-contents (package-refresh-contents))
-  (starter-kit-elpa-install))
 
 (provide 'elpa-config)
