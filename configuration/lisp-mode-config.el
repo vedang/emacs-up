6da55c888f8c24f4b4461fc5d672b14f79718f34
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-08 20:53:21 +0530

    updated header

--- configuration/lisp-mode-config.el
+++ configuration/lisp-mode-config.el
@@ -1,7 +1,21 @@
-;;; Slime
+;;; lisp-mode-config.el --- Configuration for Slime and lisp modes in general.
+;;; Author: Vedang Manerikar
+;;; Created on: 08 Jan 2012
+;;; Time-stamp: "2012-01-08 20:52:42 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
 
 (setq slime-net-coding-system 'utf-8-unix)
-
 (slime-setup)
 (add-hook 'lisp-mode-hook 'turn-on-slime)
 (add-hook 'lisp-mode-hook 'turn-on-paredit)
