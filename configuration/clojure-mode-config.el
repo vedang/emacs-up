17c15e41349b02dea3638771021bb1bab2208ef9
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-10 16:29:45 +0530

    Added header

--- configuration/clojure-mode-config.el
+++ configuration/clojure-mode-config.el
@@ -1,4 +1,19 @@
-;;; Settings for clojure
+;;; clojure-mode-config.el --- configuration for clojure
+;;; Author: Vedang Manerikar
+;;; Created on: 10 Jan 2012
+;;; Time-stamp: "2012-01-10 16:28:56 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
 
 
 ;;; Some people prefer this character. "ƒ"
