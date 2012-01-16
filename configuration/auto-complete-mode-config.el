a35dbcb9628bd484975218da72bea779039a757b
Vedang Manerikar <ved.manerikar@gmail.com>
2012-01-16 22:40:54 +0530

    Added header

--- configuration/auto-complete-mode-config.el
+++ configuration/auto-complete-mode-config.el
@@ -1,4 +1,20 @@
-;;; my settings for ac-mode
+;;; auto-complete-mode-config.el --- configuration for auto-complete-mode-mode
+;;; Author: Vedang Manerikar
+;;; Created on: 16 Jan 2012
+;;; Time-stamp: "2012-01-16 22:05:34 vedang"
+;;; Copyright (c) 2012 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
+
 
 (require 'auto-complete-config)
 
