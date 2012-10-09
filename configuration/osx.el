124e6fc0fe897624b202ea89c8245efacf849ab9
Vedang Manerikar <ved.manerikar@gmail.com>
2012-10-09 23:36:06 +0530

    Add anything-source function to read pragma marks in obj-c code.

--- configuration/osx.el
+++ configuration/osx.el
@@ -90,4 +90,25 @@
                . objc-mode))
 
 
+;; modifications for objc-mode
+(eval-after-load "objc-mode"
+  '(progn
+     (require 'anything)
+     (require 'anything-config)
+
+     (defvar anything-c-source-objc-headline
+       '((name . "Objective-C Headline")
+         (headline  "^[-+@]\\|^#pragma mark")))
+
+     (defun objc-headline ()
+       (interactive)
+       ;; Set to 500 so it is displayed even if all methods are not narrowed down.
+       (let ((anything-candidate-number-limit 500))
+         (anything-other-buffer '(anything-c-source-objc-headline)
+                                "*ObjC Headline*")))
+
+     (define-key objc-mode-map
+       (kbd "C-c p")
+       'objc-headline)))
+
 (provide 'osx)
