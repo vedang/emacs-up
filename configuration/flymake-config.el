645afc724b9b1db8261e6cbb376981d7083d45b0
Vedang Manerikar <ved.manerikar@gmail.com>
2013-09-13 16:22:20 +0530

    Add handy function to remove pesky modes from flymake's name-mask
    Pair-programmed with: Kiran Kulkarni (@kirankulkarni)

--- configuration/flymake-config.el
+++ configuration/flymake-config.el
@@ -60,7 +60,21 @@ makes)."
   (load-library "flymake-cursor")
   (global-set-key [f3] 'flymake-goto-prev-error)
   (global-set-key [f2] 'flymake-goto-next-error)
-  (delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks))
+
+  (defvar flymake-disabled-modes '("java" "xml" "html"))
+
+  (defun flymake-delete-unnecessary-modes (modes-to-disable)
+    "Remove modes from flymake-allowed-file-name-masks to avoid flymake popup."
+    (let* ((flymake-elts-to-delete))
+      (dolist (elt flymake-allowed-file-name-masks)
+        (dolist (disabled-mode modes-to-disable)
+          (when (eq (string-match-p (car elt) (concat "." disabled-mode)) 0)
+            (add-to-list 'flymake-elts-to-delete elt))))
+      (dolist (elt flymake-elts-to-delete)
+        (setq flymake-allowed-file-name-masks
+              (delete elt flymake-allowed-file-name-masks)))))
+
+  (flymake-delete-unnecessary-modes flymake-disabled-modes))
 
 
 (provide 'flymake-config)
