858c81a520feee674379f6704ec4ae1d78f62144
Vedang Manerikar <ved.manerikar@gmail.com>
2013-09-23 10:34:41 -0700

    Add smart-tab from el-get

--- el-get/.status.el
+++ el-get/.status.el
@@ -1,3 +1,4 @@
 ((el-get status "installed" recipe
 	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
- )
+ (smart-tab status "installed" recipe
+	    (:name smart-tab :description "Intelligent tab completion and indentation." :type github :pkgname "genehack/smart-tab" :features smart-tab)))
