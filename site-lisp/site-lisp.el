e52cc7da1718a5026f9c6fe00c70ad1747b22aa7
Vedang Manerikar <ved.manerikar@gmail.com>
2013-10-13 04:47:37 -0700

    Add settings for `uniquify`

--- site-lisp/site-lisp.el
+++ site-lisp/site-lisp.el
@@ -14,7 +14,10 @@
 
 ;;; Code:;;;
 
-(require 'cl)
 (require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse
+           uniquify-separator "|"
+           uniquify-after-kill-buffer-p t
+           uniquify-ignore-buffers-re "^\\*")
 
 (provide 'site-lisp)
