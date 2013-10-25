ac735bc2bf4c0b82989677dab186c0787f7c2e83
Vedang Manerikar <ved.manerikar@gmail.com>
2013-10-25 08:51:55 -0700

    Install `isearch+` from el-get

---
+++ el-get-init-files/init-isearch+.el
@@ -0,0 +1,21 @@
+;;; init-search+.el --- configuration for isearch+ and isearch-prop
+;;; Author: Vedang Manerikar
+;;; Created on: 16 Jan 2012
+;;; Copyright (c) 2012, 2013 Vedang Manerikar <vedang.manerikar@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the Do What The Fuck You Want to
+;; Public License, Version 2, which is included with this distribution.
+;; See the file LICENSE.txt
+
+;;; Code:
+
+
+(eval-after-load "isearch" '(require 'isearch+))
+
+
+(provide 'init-search+)
