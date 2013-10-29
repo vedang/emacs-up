f53f466dab069653702da2b44af9486342eec6d9
Vedang Manerikar <ved.manerikar@gmail.com>
2013-10-29 18:13:30 +0530

    Add a flymake checker function for java-programming.
    Since this is the first flymake-checker I've added, note the necessary
    steps in the README.

--- site-lisp/init-flymake.el
+++ site-lisp/init-flymake.el
@@ -16,7 +16,7 @@
 
 
 ;; I don't want flymake for these modes.
-(defvar flymake-disabled-modes '("java" "xml" "html"))
+(defvar flymake-disabled-modes '("java" "xml" "html" "c"))
 
 
 (defun if/flymake-delete-unnecessary-modes (modes-to-disable)
@@ -31,13 +31,53 @@
             (delete elt flymake-allowed-file-name-masks)))))
 
 
+;; http://git.vo20.nl/
+;; Fix for nasty `cannot open doc string file` flymake error
+(defun if/flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking FILE-NAME.
+  This is a replacement for `flymake-create-temp-inplace'. The
+  difference is that it gives a file name in
+  `temporary-file-directory' instead of the same directory as
+  FILE-NAME.
+
+  For the use of PREFIX see that function.
+
+  Note that not making the temporary file in another directory
+  \(like here) will not if the file you are checking depends on
+  relative paths to other files \(for the type of checks flymake
+  makes)."
+  (unless (stringp file-name) (error "Invalid file-name"))
+  (or prefix (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext)))
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
+
+
+;; Flymake for java.
+;; Taken from the configuration of tkj
+;; https://github.com/skybert/my-little-friends
+(defun if/flymake-javachecks-init ()
+  (list "flymake-java-checker"
+        (list (flymake-init-create-temp-buffer-copy
+               'flymake-create-temp-with-folder-structure))))
+
+
 (eval-after-load "flymake"
   '(progn
      (require 'flymake-cursor)
      (global-set-key (kbd "<f3>") 'flymake-goto-prev-error)
      (global-set-key (kbd "<f2>") 'flymake-goto-next-error)
 
-     (if/flymake-delete-unnecessary-modes flymake-disabled-modes)))
+     (if/flymake-delete-unnecessary-modes flymake-disabled-modes)
 
+     (add-to-list 'flymake-allowed-file-name-masks
+                  '("\\.java\\'"
+                    if/flymake-javachecks-init
+                    flymake-simple-cleanup))))
 
 (provide 'init-flymake)
