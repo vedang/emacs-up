5f3168d0847a7d5779915d1903d9363d80f4ab4d
Vedang Manerikar <ved.manerikar@gmail.com>
2013-11-15 15:58:43 +0530

    Add my familiar old key-binding for cider-selector
    `C-c M-s n` is too hard

--- el-get-init-files/init-cider.el
+++ el-get-init-files/init-cider.el
@@ -26,7 +26,8 @@
 (eval-after-load 'cider
   '(progn
      (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-     (add-hook 'cider-repl-mode-hook 'subword-mode)))
+     (add-hook 'cider-repl-mode-hook 'subword-mode)
+     (define-key cider-mode-map (kbd "C-c z") 'cider-selector)))
 
 
 (provide 'init-cider)
