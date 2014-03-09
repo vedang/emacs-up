bfdffd1d06e2588acc28dfcb3c2f0ef2fa84b736
Vedang Manerikar <ved.manerikar@gmail.com>
2014-03-09 16:26:17 +0530

    Add utility function to rename file and buffer in one go.

--- site-lisp/utility-functions.el
+++ site-lisp/utility-functions.el
@@ -210,4 +210,20 @@ Subsequent calls expands the selection to larger semantic unit."
      ,@clean-up))
 
 
+;; http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer/
+;; ============================================================
+(defun uf/rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+
 (provide 'utility-functions)
