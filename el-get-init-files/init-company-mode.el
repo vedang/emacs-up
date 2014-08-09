13ea6ff2067dbbc39ddb3f402feb10d4e2814b0b
Vedang Manerikar <ved.manerikar@gmail.com>
2014-08-10 00:05:41 +0530

    Fix: value for =company-idle-delay= has changed.

--- el-get-init-files/init-company-mode.el
+++ el-get-init-files/init-company-mode.el
@@ -17,7 +17,7 @@
 
 
 (add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay t
+(setq company-idle-delay 0
       company-require-match nil)
 (eval-after-load 'company
   '(progn (setq-default company-lighter " cmp")
