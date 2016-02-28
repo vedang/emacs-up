9ab69f0ef306471b799c06de16edde19004fd1ef
Vedang Manerikar <ved.manerikar@gmail.com>
2016-02-28 23:39:28 +0530

    Add a helper function to copy buffer's filename

--- site-lisp/utility-functions.el
+++ site-lisp/utility-functions.el
@@ -228,5 +228,15 @@ Subsequent calls expands the selection to larger semantic unit."
           (rename-file filename new-name t)
           (set-visited-file-name new-name t t)))))))
 
+;; From Prelude
+(defun prelude-copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
 
 (provide 'utility-functions)
