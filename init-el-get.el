0599039ea0501f8091a38a2e309ff81b350e0ff1
Vedang Manerikar <ved.manerikar@gmail.com>
2016-06-04 20:24:19 +0530

    Use ~xterm-color~ for nice colors in the terminal

--- init-el-get.el
+++ init-el-get.el
@@ -90,6 +90,10 @@
                 :after (progn (unicode-fonts-setup)))
          (:name writegood
                 :after (progn (global-set-key (kbd "C-c g") 'writegood-mode)))
+         (:name xterm-color
+                :after (progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+                              (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+                              (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)))
          (:name yasnippet
                 :after (progn (yas-global-mode 1)
                               (add-to-list 'hippie-expand-try-functions-list
