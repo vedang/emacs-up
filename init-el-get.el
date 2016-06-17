d7dee315692ee18a4f907f3fa92b36d7c81716e2
Vedang Manerikar <ved.manerikar@gmail.com>
2016-06-17 10:46:45 +0530

    Make sure ~xterm-color~ is required.

--- init-el-get.el
+++ init-el-get.el
@@ -91,7 +91,8 @@
          (:name writegood
                 :after (progn (global-set-key (kbd "C-c g") 'writegood-mode)))
          (:name xterm-color
-                :after (progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+                :after (progn (require 'xterm-color)
+                              (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
                               (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
                               (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)))
          (:name yasnippet
