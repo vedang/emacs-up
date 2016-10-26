b91ae09f64cc0cb0dff9c4eae0f38f5c6af03ab8
Vedang Manerikar <ved.manerikar@gmail.com>
2016-10-26 22:53:17 +0530

    Ask cljr to ignore errors when building asts.

--- el-get-init-files/init-clj-refactor.el
+++ el-get-init-files/init-clj-refactor.el
@@ -19,7 +19,13 @@
   (clj-refactor-mode 1)
   (cljr-add-keybindings-with-prefix "C-c m"))
 
-(setq cljr-favor-prefix-notation nil)
+(setq cljr-favor-prefix-notation nil ; don't mess up namespaces on cleanup
+      ;; don't build asts on starup, this leads to running the test
+      ;; suite on connect.
+      cljr-eagerly-build-asts-on-startup nil
+      ;; don't stop on analyzer failures. examples: not finding a
+      ;; data-reader.
+      cljr-ignore-analyzer-errors t)
 
 (eval-after-load 'clojure-mode
   '(progn
