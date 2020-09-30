1971e3265db308c33cb57c0caff5b57a502f1eba
Vedang Manerikar <ved.manerikar@gmail.com>
2020-09-30 22:49:52 +0530

    Comment out totd to stop failures in native-comp.
    I will figure out later why this particular function does not play
    well.

--- init.el
+++ init.el
@@ -203,13 +203,13 @@ CURR-THEME is the theme that gets loaded. Available values:
            (- (+ hi lo)
               (+ (first *emacs-load-start*)
                  (second *emacs-load-start*)))))
-(uf/totd)
+;; (uf/totd)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
 
 (provide 'init)
 ;;; init.el ends here
