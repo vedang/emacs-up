44015da1dcae118f214233f5fa906d082ef21253
Vedang Manerikar <ved.manerikar@gmail.com>
2020-10-26 22:10:01 +0530

    Add code to interact with the rebl
    Hat-Tip: https://github.com/admiralbumblebee/cider-rebl

--- el-get-init-files/init-cider.el
+++ el-get-init-files/init-cider.el
@@ -70,6 +70,29 @@ project."
      (define-key cider-repl-mode-map (kbd "C-M-q") 'prog-indent-sexp)
      (define-key cider-repl-mode-map (kbd "C-c M-o") 'cider-repl-clear-buffer)))
 
+;;; Integration with REBL
+;; Similar to C-x C-e, but sends to REBL
+(defun rebl-eval-last-sexp ()
+  (interactive)
+  (let* ((bounds (cider-last-sexp 'bounds))
+         (s (cider-last-sexp))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+;; Similar to C-M-x, but sends to REBL
+(defun rebl-eval-defun-at-point ()
+  (interactive)
+  (let* ((bounds (cider-defun-at-point 'bounds))
+         (s (cider-defun-at-point))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+;; C-S-x send defun to rebl
+;; C-x C-r send last sexp to rebl (Normally bound to "find-file-read-only"... Who actually uses that though?)
+(add-hook 'cider-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-S-x") #'rebl-eval-defun-at-point)
+            (local-set-key (kbd "C-x C-r") #'rebl-eval-last-sexp)))
 
 (provide 'init-cider)
 ;;; init-cider ends here
