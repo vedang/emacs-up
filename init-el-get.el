279b15f8d72fe494f1b758d46293b0db72879b10
Vedang Manerikar <ved.manerikar@gmail.com>
2020-12-01 19:28:58 +0530

    Install dired+

--- init-el-get.el
+++ init-el-get.el
@@ -179,6 +179,8 @@
          (:name deadgrep
                 :after (progn (global-set-key (kbd "C-c s") 'deadgrep)))
 
+         (:name dired+)
+
          (:name dumb-jump
                 :after (progn (dumb-jump-mode)
                               (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
