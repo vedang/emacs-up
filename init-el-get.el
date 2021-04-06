bcc143b0c5a2e87aba4826b87bab8ccd49263934
Vedang Manerikar <ved.manerikar@gmail.com>
2021-04-06 12:28:10 +0530

    Install docker and dockerfile-mode
    `docker.el` is just incredible as an integration platform with Docker.

--- init-el-get.el
+++ init-el-get.el
@@ -229,6 +229,10 @@
          (:name deadgrep
                 :after (progn (global-set-key (kbd "M-g a") 'deadgrep)))
 
+         (:name docker)
+
+         (:name dockerfile-mode)
+
          (:name dumb-jump
                 :after (progn (dumb-jump-mode)
                               (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
