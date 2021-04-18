e7f5479cdcaa51182b8dc88aeaa3a624dc8de0db
Vedang Manerikar <ved.manerikar@gmail.com>
2021-04-18 20:06:34 +0530

    Fill the year using ts-format
    - Rewrite the template to work for any week number, with week number
      taken as the input from the user
    
    - Remove the +1 in the week block.
      I don't remember why I added this, will fix it with a detailed note
      if I need to later.
    
    YASnippet is incredible, and I'm super happy with this snippet.

--- site-lisp/utility-functions.el
+++ site-lisp/utility-functions.el
@@ -328,15 +328,15 @@ argument TOPLEVEL, create a top level datetree in the file."
      'subtree-at-point)))
 
 ;;; The following code depends on `ts' library being installed.
-(defun this-week-range ()
-  "Return timestamps (BEG . END) spanning the current calendar work week.
+(require 'ts)
 
-(Mon to Fri)."
-  (let* (;; Bind `now' to the current timestamp to ensure all calculations
-         ;; begin from the same timestamp.  (In the unlikely event that
-         ;; the execution of this code spanned from one day into the next,
-         ;; that would cause a wrong result.)
-         (now (ts-now))
+(defun this-week-range (&optional week-num)
+  "Return timestamps \(BEG . END\) spanning the WEEK-NUM calendar work week.
+If WEEK-NUM is not provided, use the current week."
+  (let* ((now (ts-now))
+         ;; Navigate to the date we need to
+         (curr-week (string-to-number (ts-format "%W" now)))
+         (days-to-adjust (if week-num (* 7 (- curr-week week-num)) 0))
          ;; We start by calculating the offsets for the beginning and
          ;; ending timestamps using the current day of the week.  Note
          ;; that the `ts-dow' slot uses the "%w" format specifier, which
@@ -348,11 +348,11 @@ argument TOPLEVEL, create a top level datetree in the file."
          ;; new timestamps, so `now' is unchanged.
          (beg (thread-last now
                 ;; `ts-adjust' makes relative adjustments to timestamps.
-                (ts-adjust 'day adjust-beg-day)
+                (ts-adjust 'day (- adjust-beg-day days-to-adjust))
                 ;; `ts-apply' applies absolute values to timestamps.
                 (ts-apply :hour 0 :minute 0 :second 0)))
          (end (thread-last now
-                (ts-adjust 'day adjust-end-day)
+                (ts-adjust 'day (- adjust-end-day days-to-adjust))
                 (ts-apply :hour 23 :minute 59 :second 59))))
     (cons beg end)))
 
