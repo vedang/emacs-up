a24e1410e2bc60f5087efc0d5bc53cd7776a8d82
Vedang Manerikar <ved.manerikar@gmail.com>
2021-05-20 19:27:22 +0530

    Bring the diminish code up-to-date

--- el-get-init-files/init-diminish.el
+++ el-get-init-files/init-diminish.el
@@ -1,4 +1,4 @@
-;;; init-diminish.el - Configuration for diminish mode
+;;; init-diminish.el --- Configuration for diminish mode
 ;;; Author: Vedang Manerikar
 ;;; Created on: 05 Aug 2014
 ;;; Copyright (c) 2014 Vedang Manerikar <vedang.manerikar@gmail.com>
@@ -12,23 +12,44 @@
 ;; Public License, Version 2, which is included with this distribution.
 ;; See the file LICENSE.txt
 
-;;; Code:;;;
+;;; Commentary:
+;; Diminish is a neat way to remove clutter from the mode-line. I
+;; don't need to know that these minor modes are active, since they
+;; are an integral part of my workflow and are always active.
 
-(eval-after-load 'flyspell
-  '(diminish 'flyspell-mode))
-(eval-after-load 'paredit
-  '(diminish 'paredit-mode "P"))
-(eval-after-load 'yasnippet
-  '(diminish 'yas-minor-mode))
-(eval-after-load 'whitespace
-  '(diminish 'whitespace-mode))
-(eval-after-load 'smart-tab
-  '(diminish 'smart-tab-mode))
-(eval-after-load 'clj-refactor
-  '(diminish 'clj-refactor-mode))
-(eval-after-load 'simple
-  '(diminish 'visual-line-mode))
-(eval-after-load 'subword
-  '(diminish 'subword-mode))
-(eval-after-load 'helm-mode
-  '(diminish 'helm-mode))
+;;; Code:
+
+(with-eval-after-load 'flyspell
+  (diminish 'flyspell-mode))
+(with-eval-after-load 'paredit
+  (diminish 'paredit-mode))
+(with-eval-after-load 'yasnippet
+  (diminish 'yas-minor-mode))
+(with-eval-after-load 'whitespace
+  (diminish 'whitespace-mode))
+(with-eval-after-load 'smart-tab
+  (diminish 'smart-tab-mode))
+(with-eval-after-load 'clj-refactor
+  (diminish 'clj-refactor-mode))
+(with-eval-after-load 'simple
+  (diminish 'visual-line-mode))
+(with-eval-after-load 'subword
+  (diminish 'subword-mode)
+  (diminish 'superword-mode))
+(with-eval-after-load 'helm-mode
+  (diminish 'helm-mode))
+(with-eval-after-load 'projectile
+  (diminish 'projectile-mode))
+(with-eval-after-load 'hideshow
+  (diminish 'hs-minor-mode))
+(with-eval-after-load 'noutline
+  (diminish 'outline-mode))
+(with-eval-after-load 'ansible-doc
+  (diminish 'ansible-doc-mode))
+(with-eval-after-load 'autorevert
+  (diminish 'auto-revert-mode))
+(with-eval-after-load 'company
+  (diminish 'company-mode))
+
+(provide 'init-dimish)
+;;; init-diminish.el ends here
