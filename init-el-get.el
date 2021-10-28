fbbccc72b01a18f452fc7b67a08b0105d8dd0d3f
Vedang Manerikar <ved.manerikar@gmail.com>
2021-10-28 22:57:57 +0530

    Use a light theme for some time.
    I should probably remove this part of the config from version control,
    so that I don't have useless commits like this one in the repo

--- init-el-get.el
+++ init-el-get.el
@@ -329,7 +329,7 @@
                 :after (progn ;; Global settings (defaults)
                          (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
                                doom-themes-enable-italic t) ; if nil, italics is universally disabled
-                         (load-theme 'doom-vibrant t)
+                         (load-theme 'doom-one-light t)
 
                          ;; Enable flashing mode-line on errors
                          (doom-themes-visual-bell-config)
