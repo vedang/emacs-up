8af988a35671c3b62ff4ded32a30871cc1632346
Vedang Manerikar <ved.manerikar@gmail.com>
2022-09-08 17:46:10 +0530

    Re-arrange utf-8 settings
    This was an attempt to fix Devanagari rendering on my machine,
    following the Mastering Emacs blog. But it did not work.
    
    What eventually worked was installing the correct Devanagari font on
    my Mac OS. I cannot explain why this was working earlier, broke, and
    then was fixed by installing a font. But it was.

--- site-lisp/core.el
+++ site-lisp/core.el
@@ -79,13 +79,19 @@
 
 ;;; Everything in UTF8
 (prefer-coding-system 'utf-8)
-(set-language-environment 'UTF-8)
 (set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(when (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8))
+(when (boundp 'default-buffer-file-coding-system)
+  (setq default-buffer-file-coding-system 'utf-8))
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
 (setq file-name-coding-system 'utf-8)
 (setq buffer-file-coding-system 'utf-8)
 (setq coding-system-for-write 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
+
 (set-clipboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (setq default-process-coding-system '(utf-8 . utf-8))
