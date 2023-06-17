02365ac99762c18efe84353152bd15fab0d77a59
Vedang Manerikar <ved.manerikar@gmail.com>
2023-06-17 10:01:04 +0530

    Add a key-binding for find-dired
    Note that this uses the same keybinding as dired+, which I have
    stopped using.

--- site-lisp/site-lisp.el
+++ site-lisp/site-lisp.el
@@ -39,6 +39,8 @@
     (interactive)
     (eww-open-file (dired-get-file-for-visit))))
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+(global-set-key (kbd "C-x D") #'find-dired)
+
 ;;; commented out to see if this is causing problems in Emacs 29
 ;; (require 'dired+)
 
@@ -64,24 +66,24 @@ right mode to start in the buffer."
 
 
 ;; Bindings
-(global-set-key (kbd "M-j") 'pop-to-mark-command)
-(global-set-key (kbd "RET") 'reindent-then-newline-and-indent)
-(global-set-key (kbd "A-l") 'goto-line)
-(global-set-key (kbd "C-x n r") 'narrow-to-region)
-(global-set-key (kbd "C-x \\") 'align-regexp)
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-c y") 'bury-buffer)
-(global-set-key (kbd "<f5>") 'revert-buffer)
-(global-set-key (kbd "C-x m") 'eshell)
-(global-set-key (kbd "C-x M") 'shell)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-(global-set-key (kbd "C-S-t") 'transpose-sexps)
+(global-set-key (kbd "M-j") #'pop-to-mark-command)
+(global-set-key (kbd "RET") #'reindent-then-newline-and-indent)
+(global-set-key (kbd "A-l") #'goto-line)
+(global-set-key (kbd "C-x n r") #'narrow-to-region)
+(global-set-key (kbd "C-x \\") #'align-regexp)
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "C-c y") #'bury-buffer)
+(global-set-key (kbd "<f5>") #'revert-buffer)
+(global-set-key (kbd "C-x m") #'eshell)
+(global-set-key (kbd "C-x M") #'shell)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-+") #'text-scale-increase)
+(global-set-key (kbd "C--") #'text-scale-decrease)
+(global-set-key (kbd "C-S-t") #'transpose-sexps)
 ;; M-q is eaten inside VirtualBox on Mac.
 ;; Re-mapping some important functions which depend on M-q
-(global-set-key (kbd "C-c q") 'fill-paragraph)
-(global-set-key (kbd "C-s-q") 'prog-indent-sexp)
+(global-set-key (kbd "C-c q") #'fill-paragraph)
+(global-set-key (kbd "C-s-q") #'prog-indent-sexp)
 
 
 ;; Aliases for common functions
